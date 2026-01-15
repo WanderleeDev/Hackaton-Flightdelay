321d5ff97f82d62103f01f9a8b25b373f43ee7f1
Wanderlee <wanderleedev@gmail.com>
2026-01-14 21:21:10 -0500

    chore: Remove old flight-ontime backup directory and its contents.

--- backups/flight-ontime-backup-20260107_161226/src/main/java/com/hackathon/flight_ontime/dto/DataResponse.java
+++
@@ -1,7 +0,0 @@
-package com.hackathon.flight_ontime.dto;
-
-public record DataResponse(
-        Double probability,
-        String forecast
-) {
-}
--- backups/flight-ontime-backup-20260107_161226/src/main/java/com/hackathon/flight_ontime/predict/DTO/DataResponse.java
+++
@@ -1,7 +0,0 @@
-package com.hackathon.flight_ontime.predict.DTO;
-
-public record DataResponse(
-        Double probability,
-        String forecast
-) {
-}
