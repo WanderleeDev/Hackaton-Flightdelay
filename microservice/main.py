afaed85fa70fde8ebf4d26ead4770d21e87f9268
Wanderlee <wanderleedev@gmail.com>
2026-01-17 17:59:25 -0500

    build: Add Dockerfile for application containerization and remove the custom ping module.

--- microservice/main.py
+++ microservice/main.py
@@ -1,5 +1,3 @@
-import threading
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from slowapi import _rate_limit_exceeded_handler
@@ -7,8 +5,6 @@ from slowapi.errors import RateLimitExceeded
 from src.core import _global_error_handler, limiter
 from src.health_check import health_router
 from src.predict import predict_router
-from src.ping import  ping_router
-from src.ping.service import wake_up
 
 
 app = FastAPI(description="API for predicting individual or batch flights")
@@ -25,6 +21,3 @@ app.add_exception_handler(Exception, _global_error_handler)
 
 app.include_router(health_router)
 app.include_router(predict_router)
-app.include_router(ping_router)
-
-threading.Thread(target=wake_up, name="ping").start()
--- microservice/src/ping/__init__.py
+++
@@ -1,7 +0,0 @@
-from .router import ping_router
-from .service import wake_up
-
-__all__ = [
-    "ping_router",
-    "wake_up",
-]
